6ab6a3814fa9e540d807e295f615e9173a4921df
Joshua George <george3191@students.d211.org>
2024-08-23 11:05:18 -0500

    MobileAPPS

--- BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,9 +38,30 @@ class MainActivity : ComponentActivity() {
                         about = "Android Developer",
                         modifier = Modifier.padding(16.dp) // Use a specific padding value
                     )
+                    GreetingImage(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
+    }
+}
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.josh)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+//        GreetingText(
+//            message = message,
+//            from = from,
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(8.dp)
+//        )
     }
 }
 
@@ -47,8 +72,14 @@ fun Greeting(name: String, about: String, modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         Text(
-            text = "$name\n$about",
+            text = "$name",
             fontSize = 48.sp,
+            lineHeight = 64.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "          $about",
+            fontSize = 20.sp,
             lineHeight = 64.sp,
             textAlign = TextAlign.Center
         )
